424f24dbf265a53980cfb59364aa64d61cc4ff5f
vicenteBerrios <vicentesilverwolf@gmail.com>
2025-05-06 09:11:18 -0400

    update

--- 16 JS/script.js
+++ 16 JS/script.js
@@ -1,2 +1,13 @@
 //Prueba de conexion js!
 console.log("Conexion establecida con js!");
+
+//Creacion de variables
+
+let num = 25; // Variable INT (Numero entero)
+
+// Creacion de numeros enteros 
+function numeroEntero() {
+    return "Variable Numerica: " + num;
+}
+// Impresion en consola
+console.log(numeroEntero()); // Mostrar Numero Entero
