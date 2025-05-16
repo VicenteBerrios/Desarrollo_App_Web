11b32105519444bc84245fc17abd878379285819
vicenteBerrios <vicentesilverwolf@gmail.com>
2025-05-16 12:58:44 -0400

    update

--- 18 arreglos/script.js
+++ 18 arreglos/script.js
@@ -1,28 +1,42 @@
-const arreglo = [10, 20, 30, 40, 50, 60]; 
-
-function promedioTresPrimeros() {
-  const promedio = (arreglo[0] + arreglo[1] + arreglo[2]) / 3;
-  alert("El promedio de los tres primeros elementos es " + promedio);
+function ejercicio1() {
+  let palabras = ["Juan", "corre", "rapidamente", "feliz", "parque"];
+  let frase = palabras[0] + " " + palabras[1] + " " + palabras[2] + " en el " + palabras[4] + " " + palabras[3];
+  alert(frase);
 }
 
-function sumaExtremos() {
-  const suma = arreglo[0] + arreglo[arreglo.length - 1];
-  alert("La suma del primer y último elemento es " + suma);
+function ejercicio2() {
+  let datos = [3, "gatos", "negros", "duermen", "tranquilamente"];
+  let frase = "Los " + datos[0] + " " + datos[1] + " " + datos[2] + " " + datos[3] + " " + datos[4];
+  alert(frase);
 }
 
-function diferenciaCentroExtremos() {
-  const medio = Math.floor(arreglo.length / 2);
-  const sumaExtremos = arreglo[0] + arreglo[arreglo.length - 1];
-  const diferencia = arreglo[medio] - sumaExtremos;
-  alert("La diferencia entre el elemento central y los extremos es " + diferencia);
+function ejercicio3() {
+  let matriz = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+  ];
+  let suma = matriz[0][1] + matriz[2][0]; 
+  alert("La suma de 20 y 70 es " + suma);
 }
 
-function multiplicaPares() {
-  const producto = arreglo[0] * arreglo[2] * arreglo[4];
-  alert("El producto de las posiciones pares (0,2,4) es " + producto);
+function ejercicio4() {
+  let datos = [
+    [1, 2, 3, 4],
+    [5, 6, [7, 8, [9, 10]]]
+  ];
+  let valor1 = datos[0][2];
+  let valor2 = datos[1][2][2][1];
+  let suma = valor1 + valor2;
+  alert("La suma de " + valor1 + " y " + valor2 + " es " + suma);
 }
 
-function mensajeSuma() {
-  const suma = arreglo[1] + arreglo[3];
-  alert("La suma del segundo y cuarto elemento es " + suma + " Gran trabajo con los arreglos");
+function ejercicio5() {
+  let sujeto = ["El perro", "El gato"];
+  let acciones = [
+    ["ladra", "corre"],
+    ["duerme", "salta"]
+  ];
+  let frase = sujeto[1] + " " + acciones[1][0] + ".";
+  alert(frase);
 }
