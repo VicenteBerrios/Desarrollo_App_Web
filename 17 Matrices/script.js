f41b2a96d876e81ea129365ec674c292b5bc369a
vicenteBerrios <vicentesilverwolf@gmail.com>
2025-05-16 12:21:32 -0400

    update

---
+++ 17 Matrices/script.js
@@ -0,0 +1,5 @@
+const dias = ["lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"];
+
+document.getElementById("btn").onclick = function() {
+  alert("El segundo día de la semana es: " + dias[1]);
+};
