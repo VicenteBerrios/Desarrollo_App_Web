8538a195cc487875bcd06f3e1502dedc0df228f2
vicenteBerrios <vicentesilverwolf@gmail.com>
2025-06-05 08:57:15 -0400

    update

--- 20 Metodos/script.js
+++ 20 Metodos/script.js
@@ -1,26 +1,26 @@
+// Creación de Arreglo
 let array = ["Xbox","Nintendo"];
-
+//Creación de funciones
 function usarPush(){
+// Agregar al final
     let entrada = prompt("Agregar una consola de juego: ");
     array.push(entrada);
-    alert("El nuevo arreglo es: " + array.join(" "));
+    alert("El nuevo arreglo es: " + array.join(" - "));
 }
-
 function usarPop(){
+    //Elimina el último elemento
     let basurero = array.pop();
-    aler(`Elemento eliminado: ${basurero}`);
+    alert(`Elemento eliminado: ${basurero}`);
 }
-
 function usarUnshift(){
+    // Agregar al inicio
     let entrada = prompt("Agregar una consola de juego: ");
     array.unshift(entrada);
     alert("El nuevo arreglo es: " + array.join(" "));
 }
-
 function usarShift(){
-    let basurero = array.shift();
-    aler(`Elemento eliminado: ${basurero}`);
+        //Elimina el primer elemento
+        let basurero = array.shift();
+        alert(`Elemento eliminado: ${basurero}`);
 }
-
-
 
