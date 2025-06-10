34bf29be53e3396fee1deba72e7b4445d5a113e3
vicenteBerrios <vicentesilverwolf@gmail.com>
2025-06-10 09:27:16 -0400

    Update

--- 21 Trabajo/script.js
+++ 21 Trabajo/script.js
@@ -1,14 +1,47 @@
-function mayoriaEdad(){
+/*✅ Ejercicio 1: Verificar mayoría de edad
+Crea una variable edad con un valor numérico.
+Si la edad es mayor o igual a 18, muestra
+"Acceso permitido".
+ Si no, muestra "Acceso denegado". */
+
+ function mayoriaEdad(){
     let edad = parseInt(prompt("Ingrese Edad: "));
     if(edad >= 18){
-        document.getElementsById("resultado").innerHTML =
-        <P>Es mayor de Edad: ${edad}</P>;
-    }else if(edad <= 17 && edad >= 0){
         document.getElementById("resultado").innerHTML =
-        <P>Es menor de Edad: ${edad}</P>;
-    }else{
+        `<p>Es mayor de Edad: ${edad} </p>`;
+    }else if (edad <= 17 && edad >= 0){
         document.getElementById("resultado").innerHTML =
-        <P>El valor <strong>${edad}</strong> es invalido</P>;
+        `<p>Es menor de Edad: ${edad} </p>`;
+    } else {
+        document.getElementById("resultado").innerHTML =
+        `<p>El valor <strong>${edad}</strong> es invalido.</p>`;
     }
 }
 
+
+/* ✅ Ejercicio 2: Añadir producto si no está
+js
+let productos = ["pan", "leche", "queso"];
+
+Declara la variable nuevoProducto.
+Si productos no incluye el nuevo producto (agrégalo con push() y muestra el arreglo.
+Si ya está, muestra "Producto ya ingresado". */
+function mostrarNuevoProducto(){
+    let productos = ["pan", "leche", "queso"];
+    let nuevoProducto = prompt("Ingrese un nuevo Producto");
+    if (productos[0] == nuevoProducto){
+        document.getElementById("resultado").innerHTML =
+        `<p>El producto: <strong>${nuevoProducto}</strong> ingresado.</p>`;
+    } else if(productos[1] == nuevoProducto){
+        document.getElementById("resultado").innerHTML =
+        `<p>El producto: <strong>${nuevoProducto}</strong> ingresado.</p>`;
+    } else if(productos[2] == nuevoProducto){
+        document.getElementById("resultado").innerHTML =
+        `<p>El producto: <strong>${nuevoProducto}</strong> ingresado.</p>`;
+} else{
+        productos.push(nuevoProducto);
+        document.getElementById("resultado").innerHTML =
+        `<p>Nueva lista de productos: <strong>${productos.join(" - ")}</strong> ingresado.</p>`;
+}
+
+}
